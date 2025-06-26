eada396271006ba985f4583ade50e03c3df480d8
emarcosgv <emarcosgv@gmail.com>
2025-06-26 15:04:11 -0300

    commit inicial

---
+++ src/main/java/com/days/apidays/ApiDAySApplication.java
@@ -0,0 +1,13 @@
+package com.days.apidays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDAySApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDAySApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/days/apidays/ApiDAySApplicationTests.java
@@ -0,0 +1,13 @@
+package com.days.apidays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDAySApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
