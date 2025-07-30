9972e00966b06d4e0559394d1e66054a7dcf44a6
emarcosgv <emarcosgv@gmail.com>
2025-07-30 16:54:07 -0300

    funcionando el tomcast

--- src/main/java/controller/DelitoController.java
+++ src/main/java/controller/DelitoController.java
@@ -1,10 +1,33 @@
 package controller;
 
+import controller.response.CustomError;
+import dto.delincuente.DelincuenteOutputDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Tag(name="Delitos", description = "Delito operaciones")
 public class DelitoController extends BaseController{
+
+    @GetMapping(value = "/delincuente/{id}")
+    // @Operation(summary = "Get a delicuente", description = "Get a delincuente") // Comentado o eliminado
+    // @ApiResponses(value = { // Comentado o eliminado
+    //     @ApiResponse(responseCode = "200", description = "OK", content = { // Comentado o eliminado
+    //         @Content(mediaType = "application/json", schema = @Schema(implementation = DelincuenteOutputDTO.class)) // Comentado o eliminado
+    //     }) // Comentado o eliminado
+    // }) // Comentado o eliminado
+    public String getDelincuente(@PathVariable Long id) { // Cambiado el tipo de retorno a String y el parámetro si es necesario
+        return "Hello World"; // Retorna la cadena "Hello World"
+    }
+
+
+
 
 }
---
+++ src/main/java/dto/delincuente/DelincuenteOutputDTO.java
@@ -0,0 +1,4 @@
+package dto.delincuente;
+
+public class DelincuenteOutputDTO {
+}
